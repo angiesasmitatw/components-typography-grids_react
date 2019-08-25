c8a1717b8c7e68c5370e92ab456dcc3ffb0027e3
Angelita Sasmita <angelita.sasmita@thoughtworks.com>
2019-08-25 18:38:19 +0800

    Add biggest screen styling

--- src/layout/SignUp.jsx
+++ src/layout/SignUp.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useMediaPredicate } from 'react-media-hook';
-import { GridContainer, InputBox, Flex, Button } from '../components/commons';
+import { GridContainer, Flex, Button } from '../components/commons';
 import Logo from '../components/images/logo.png';
+import Product from '../components/images/product.png';
 import { Colors, Typography, Breakpoints } from '../themes';
 import { HelperText, TitleText, SignUpInputBox } from '../components/SignUp';
 
@@ -42,6 +43,9 @@ const BigScreenBox = styled.div`
     border-radius: 5px;
     border-style: solid;
   }
+  @media ${Breakpoints.md} {
+    border: none;
+  }
 `;
 
 const BigScreenContainer = styled.div`
@@ -53,49 +57,83 @@ const BigScreenContainer = styled.div`
   }
 `;
 
+const BigScreenExtension = styled.div`
+  @media ${Breakpoints.md} {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 800px;
+    height: 670px;
+    padding-right: 80px;
+    justify-content: center;
+    border-color: ${Colors.grey};
+    border-width: 1px;
+    border-radius: 5px;
+    border-style: solid;
+  }
+`;
+
+const BigScreenImage = styled.div`
+  display: none;
+  @media ${Breakpoints.md} {
+    display: flex;
+    align-items: center;
+  }
+`;
+
 const SignUpPage = () => {
   const biggerThanSmallScreen = useMediaPredicate(`(${Breakpoints.sm})`);
   return (
     <BigScreenContainer>
-      <BigScreenBox>
-        <GridContainer alignContent="flex-start">
-          <LogoContainer>
-            <img src={Logo} alt="Company Logo" />
-          </LogoContainer>
-          <TitleText text="Create your Diabeetus Account" />
-          <SignUpInputBox
-            placeholderText="First Name"
-            isAlwaysFullLengthField={false}
-            isFirstHalf
+      <BigScreenExtension>
+        <BigScreenBox>
+          <GridContainer alignContent="flex-start">
+            <LogoContainer>
+              <img src={Logo} alt="Company Logo" />
+            </LogoContainer>
+            <TitleText text="Create your Diabeetus Account" />
+            <SignUpInputBox
+              placeholderText="First Name"
+              isAlwaysFullLengthField={false}
+              isFirstHalf
+            />
+            <SignUpInputBox
+              placeholderText="Last Name"
+              isAlwaysFullLengthField={false}
+              isFirstHalf={false}
+            />
+            <SignUpInputBox placeholderText="Username" />
+            <HelperText text="You can use letters, numbers & periods" />
+            <SignUpInputBox
+              placeholderText="Password"
+              isAlwaysFullLengthField={false}
+              isFirstHalf
+            />
+            {!biggerThanSmallScreen && (
+              <HelperText text="Use 8 or more characters with a mix of letters, numbers & symbols" />
+            )}
+            <SignUpInputBox
+              placeholderText="Confirm"
+              isAlwaysFullLengthField={false}
+              isFirstHalf={false}
+            />
+            {biggerThanSmallScreen && (
+              <HelperText text="Use 8 or more characters with a mix of letters, numbers & symbols" />
+            )}
+            <ButtonGridPosition>
+              <StyledButton text="Confirm" />
+            </ButtonGridPosition>
+          </GridContainer>
+        </BigScreenBox>
+        <BigScreenImage>
+          <img
+            src={Product}
+            alt="Product Illustration"
+            height="300px"
+            width="300px"
           />
-          <SignUpInputBox
-            placeholderText="Last Name"
-            isAlwaysFullLengthField={false}
-            isFirstHalf={false}
-          />
-          <SignUpInputBox placeholderText="Username" />
-          <HelperText text="You can use letters, numbers & periods" />
-          <SignUpInputBox
-            placeholderText="Password"
-            isAlwaysFullLengthField={false}
-            isFirstHalf
-          />
-          {!biggerThanSmallScreen && (
-            <HelperText text="Use 8 or more characters with a mix of letters, numbers & symbols" />
-          )}
-          <SignUpInputBox
-            placeholderText="Confirm"
-            isAlwaysFullLengthField={false}
-            isFirstHalf={false}
-          />
-          {biggerThanSmallScreen && (
-            <HelperText text="Use 8 or more characters with a mix of letters, numbers & symbols" />
-          )}
-          <ButtonGridPosition>
-            <StyledButton text="Confirm" />
-          </ButtonGridPosition>
-        </GridContainer>
-      </BigScreenBox>
+        </BigScreenImage>
+      </BigScreenExtension>
     </BigScreenContainer>
   );
 };
